737ef94eae68c3383547dbe11e516bc3c18d5bff
Merlena <merlenajose.2003@gmail.com>
2026-07-05 11:16:15 +0530

    user block and unblock in controller

--- backend/src/controllers/AdminAuthController.ts
+++ backend/src/controllers/AdminAuthController.ts
@@ -38,4 +38,26 @@ import { HttpStatus } from "../constants/HttpStatus.js";
                 next(error)
             }
         }
+
+
+        blockUser=async (req:Request,res:Response,next:NextFunction) => {
+            try {
+                const userId=req.params.id as string;
+                const result=await this.adminService.blockUser(userId);
+                successResponse(res,"User is blocked Successfully",result,HttpStatus.OK);
+
+            } catch (error) {
+                next(error)
+            }
+        }
+
+        unblockUser=async (req:Request,res:Response,next:NextFunction) => {
+            try {
+               const userId=req.params.id as string;
+               const result=await this.adminService.unblockUser(userId);
+               successResponse(res,"User is unblocked successfully",result,HttpStatus.OK) 
+            } catch (error) {
+                next(error)
+            }
+        }
     }
