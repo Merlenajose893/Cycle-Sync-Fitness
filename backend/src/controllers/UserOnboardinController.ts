a7886e8594559919ae5875b74a5ed7186740dc2a
Merlena <merlenajose.2003@gmail.com>
2026-07-08 16:56:22 +0530

    user onboarding fully completed

--- backend/src/controllers/UserOnboardinController.ts
+++ backend/src/controllers/UserOnboardinController.ts
@@ -36,11 +36,13 @@ export class UserOnboardingControlling{
         }
     }
 
-    async updateCycleDetails(req:Request,res:Response,next:NextFunction)
+ updateCycleDetails=async(req:Request,res:Response,next:NextFunction)=>
     {
         try {
             const userId=req.user?.userId!;
             const result=await this.useronboardingService.updateCycleSetUp(userId,req.body);
+            console.log(result);
+            
             successResponse(res,"Cycle details are updated",result)
         } catch (error) {
             next(error);
@@ -70,9 +72,11 @@ export class UserOnboardingControlling{
 
     }
 
-    async completeOnboarding(req:Request,res:Response,next:NextFunction)
+    completeOnboarding=async(req:Request,res:Response,next:NextFunction)=>
     {
         const userId=req.user?.userId
+        console.log(userId);
+        
         const result=await this.useronboardingService.completeOnboarding(userId!)
         successResponse(res,"Completed Onboarding",result)
     }
