e22b302aeb13373b16c82c1a2590ee1d91898b2d
Merlena <merlenajose.2003@gmail.com>
2026-07-16 18:53:22 +0530

    everything added

--- backend/src/routes/userProfileRoutes.ts
+++ backend/src/routes/userProfileRoutes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { container } from "tsyringe";
 import { UserProfileController } from "../controllers/UserProfileController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { validate } from "../middlewares/validate.js";
 import { upload } from "../middlewares/upload.js";
+import { updateUserProfileSchema } from "../validators/userprofile.validator.js";
 const router=Router();
 const userprofilecontroller=container.resolve(UserProfileController);
 
 router.get("/profile",authMiddleware,userprofilecontroller.getProfile);
-router.patch("/profile",authMiddleware,userprofilecontroller.updateProfile);
+router.patch("/profile",authMiddleware,validate(updateUserProfileSchema),userprofilecontroller.updateProfile);
 router.patch("/profile/avatar",authMiddleware,upload.single("avatar"),userprofilecontroller.uploadAvatar);
 router.delete("/profile/avatar",authMiddleware,userprofilecontroller.deleteAvatar);
