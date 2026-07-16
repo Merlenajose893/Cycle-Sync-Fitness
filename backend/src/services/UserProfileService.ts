b6a756f1b4f18b0f7547c06792d5666f77b56a25
Merlena <merlenajose.2003@gmail.com>
2026-07-16 12:13:15 +0530

    feat(profile): implement profile services completely

--- backend/src/services/UserProfileService.ts
+++ backend/src/services/UserProfileService.ts
@@ -6,9 +6,10 @@ import type { IUserProfileService } from "../interfaces/services/IUserProfileSer
 import { injectable,inject } from "tsyringe";
 import { UserProfileMapper } from "../mappers/UserProfileMappers.js";
 import type { IUser } from "../models/User.js";
+import type { IImageService } from "../interfaces/services/IImageService.js";
 @injectable()
 export class UserProfileService implements IUserProfileService{
-    constructor(@inject(TOKENS.IUserRepository) private userRepository:IUserRepository )
+    constructor(@inject(TOKENS.IUserRepository) private userRepository:IUserRepository @inject(TOKENS.IImageService) private imageService:IImageService)
     {
 
     }
@@ -42,10 +43,42 @@ uploadAvatar=async(userId: string, file: Express.Multer.files): Promise<UserProf
     {
         throw new NotFoundError("User Not Found");
     }
+    if(user.avatarPublicId)
+    {
+        await this.imageService.deleteImage(user.avatarPublicId)
+    }
 
+    const uploadedImage=await this.imageService.uploadImage(file);
+    const updatedData=UserProfileMapper.toUpdateEntity({
+        avatarPublicId:uploadedImage.publicId,
+        avatarUrl:uploadedImage.url
+    })
+    const updatedUser=await this.userRepository.updateProfile(userId,updatedData);
+    if(!updatedUser)
+    {
+        throw new NotFoundError("User Not Found")
+    }
+return UserProfileMapper.toResponseDTO(updatedUser);
 }
 
-deleteAvatar(userId: string): Promise<UserProfileResponseDTO> {
+deleteAvatar=async(userId: string): Promise<UserProfileResponseDTO> =>{
+    const user=await this.userRepository.findById(userId);
+    if(!user)
+    {
+        throw new NotFoundError("User not Found");
+    }
+
+    if(!user.avatarPublicId)
+    {
+        throw new NotFoundError("Avatar not Found");
+    }
+    await this.imageService.deleteImage(user.avatarPublicId);
+    const updatedUser=await this.userRepository.updateProfile(userId,UserProfileMapper.toUpdateEntity({avatarUrl:undefined,avatarPublicId:undefined}))
+     if (!updatedUser) {
+        throw new NotFoundError("User not found");
+    }
+
     
+    return UserProfileMapper.toResponseDTO(updatedUser);
 }
 }
