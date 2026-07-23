e5695d872619af4b2cf4cedf84a6632338f82980
Merlena <merlenajose.2003@gmail.com>
2026-07-23 11:28:58 +0530

    feat(food): implement meal log repository

--- backend/src/repositories/MealLogRepository.ts
+++ backend/src/repositories/MealLogRepository.ts
@@ -1,7 +1,9 @@
+import { inject, injectable } from "tsyringe";
 import type { IMealLogRepository } from "../interfaces/repositories/IMealLogRepository.js";
 import { MealLog, type IMealLog } from "../models/MealLog.js";
 import { BaseRepository } from "./BaseRepository.js";
 import { Model } from "mongoose";
+@injectable()
 export class MealLogRepository extends BaseRepository<IMealLog> implements IMealLogRepository{
 
 
@@ -15,6 +17,6 @@ return this.model.findOne({userId,date})
     
 }
 findByUserDateRange(userId: string, startDate: Date, endDate: Date): Promise<IMealLog[]> {
-    
+    return this.model.find({userId,date:{$gte:startDate,$lte:endDate}}).sort({date:1});
 }
 }
