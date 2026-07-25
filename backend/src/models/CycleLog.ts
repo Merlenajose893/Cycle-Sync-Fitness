2f125a25854ea0d954e2d9fc665e163afb69d04d
Merlena <merlenajose.2003@gmail.com>
2026-07-25 09:24:52 +0530

    feat(models): add CycleLog model

---
+++ backend/src/models/CycleLog.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema,  Types } from "mongoose";
+import { FlowIntensity } from "../constants/health.constant.js";
+
+
+export interface ICycleLog extends Document{
+    userId:Types.ObjectId;
+    startDate:Date;
+    endDate?:Date;
+    flowIntensity:FlowIntensity;
+    notes?:string;
+    createdAt:Date;
+    updatedAt:Date;
+}
+
+const CycleLogSchema=new Schema<ICycleLog>({
+userId:{
+type:Schema.Types.ObjectId,
+ref:"User",
+required:true
+},
+startDate:{
+type:Date,
+required:true
+},
+endDate:{
+type:Date
+},
+flowIntensity:{
+type:String,
+enum:Object.values(FlowIntensity),
+required:true
+},
+notes:{
+type:String,
+
+}
+
+
+},{timestamps:true})
+
+
+export const CycleLog=mongoose.model<ICycleLog>("CycleLog",CycleLogSchema);
