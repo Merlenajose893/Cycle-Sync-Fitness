e250c32af84b0a214cd0180ba4381d717aa22a5e
Merlena <merlenajose.2003@gmail.com>
2026-07-25 14:49:47 +0530

    feat(repository): implement CycleLogRepository

---
+++ backend/src/repositories/CycleLogRepository.ts
@@ -0,0 +1,20 @@
+import type { ICycleLogRepository } from "../interfaces/repositories/ICycleLogRepository.js";
+import { CycleLog, type ICycleLog } from "../models/CycleLog.js";
+import { BaseRepository } from "./BaseRepository.js";
+
+export class CycleLogRepository extends BaseRepository<ICycleLog> implements ICycleLogRepository{
+    constructor()
+    {
+        super(CycleLog)
+    }
+
+    findByUser(userId: string): Promise<ICycleLog[]> {
+        return this.model.find({userId}).sort({startedAt:-1});
+    }
+    findByLatest(userId: string): Promise<ICycleLog | null> {
+        return this.model.findOne({userId})
+    }
+    findRecentCycles(userId: string, limit: number): Promise<ICycleLog[]> {
+        return this.model.find({userId}).sort({startedAt:-1}).limit(limit)
+    }
+}
