471330815517bc0091da9e42e766f4ec189011e1
Merlena <merlenajose.2003@gmail.com>
2026-08-01 10:14:45 +0530

    feat: add payment model

---
+++ backend/src/constants/payment.ts
@@ -0,0 +1,12 @@
+export enum PaymentStatus{
+    PENDING="PENDING",
+    COMPLETED="COMPLETED",
+    FAILED="FAILED",
+    REFUNDED="REFUNDED"
+}
+
+export enum PaymentMethod{
+    CARD="CARD",
+    UPI="UPI",
+    WALLET="WALLET"
+}
---
+++ backend/src/models/Payment.ts
@@ -0,0 +1,66 @@
+import mongoose, { Schema,Types,Document } from "mongoose";
+import { PaymentMethod, PaymentStatus } from "../constants/payment.js";
+export interface IPayment extends Document{
+    paymentId:string;
+    userId:Types.ObjectId;
+    trainerId:Types.ObjectId;
+    packageId:Types.ObjectId;
+    stripeSessionId:string;
+    amount:number;
+    currency:string;
+    paymentStatus:PaymentStatus;
+    paymentMethod:PaymentMethod;
+    createdAt:Date;
+    updatedAt:Date;
+}
+
+const PaymentSchema=new Schema<IPayment>({
+paymentId:{
+    type:String,
+    required:true
+},
+userId:{
+    type:String,
+    ref:"User",
+    required:true
+},
+trainerId:{
+    type:Schema.Types.ObjectId,
+    ref:"Trainer",
+    required:true
+},
+packageId:{
+type:Schema.Types.ObjectId,
+ref:"TrainerPackage",
+required:true
+},
+stripeSessionId:{
+    type:String,
+    required:true,
+    unique:true
+},
+amount:{
+type:Number,
+required:true
+},
+currency:{
+type:String,
+required:true,
+default:"INR"
+},
+paymentStatus:{
+type:String,
+enum:Object.values(PaymentStatus),
+default:PaymentStatus.PENDING,
+required:true
+},
+paymentMethod:{
+type:String,
+enum:Object.values(PaymentMethod),
+required:false
+}
+
+
+},{timestamps:true});
+
+export const Payment=mongoose.model<IPayment>("Payment",PaymentSchema);
