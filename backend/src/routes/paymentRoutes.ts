58a0651bb050fd55725e511fdba13023ff335d09
Merlena <merlenajose.2003@gmail.com>
2026-08-03 16:40:16 +0530

    feat(payment): integrate payment routes and refactor PaymentController

---
+++ backend/src/routes/paymentRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import express from "express"
+import { container } from "tsyringe";
+import { PaymentController } from "../controllers/PaymentController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+const paymenyController=container.resolve(PaymentController);
+const router=Router();
+router.post("/webhook",express.raw({type:"application/json"}),paymenyController.handleWebhook);
+router.post("/checkout-session",authMiddleware,paymenyController.createCheckoutSession);
+router.get("/my-payments",authMiddleware,paymenyController.getPaymentsByUser)
+router.get("/id",authMiddleware,paymenyController.getPaymentById);
